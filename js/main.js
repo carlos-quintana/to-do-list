cfe8bd0842b8101f57d5858ad852644a236437c8
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2022-03-29 15:54:55 -0500

    First iteration of the application. Implemented adding new tasks functionality.

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const form = document.querySelector("#new-task-form");
+const tasksList = document.querySelector("#tasks-list");
+
+// new-task-form is submitted
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    console.log("The New Task Form was submitted")
+    submitTask(event)
+})
+
+// Validate, sanitize and submit the task
+function submitTask() {
+    let taskDescriptionInput = retrieveTaskInput();
+    taskDescriptionInput = taskDescriptionInput.trim();
+    if (isInputEmpty(taskDescriptionInput)) {
+        alert("The new task cannot be empty");
+        console.log("The task submitted was invalid");
+    } else {
+        addTask(taskDescriptionInput);
+        resetNewTaskForm();
+        console.log(`The task "${taskDescriptionInput}" was submitted successfully`);
+    }
+}
+
+function retrieveTaskInput() {
+    return document.querySelector("#new-task-description").value
+}
+
+function isInputEmpty(input) {
+    return input === "";
+}
+
+function addTask(taskDescription) {
+    const newRow = document.createElement("li");
+    newRow.className = "task-row";
+    console.log(tasksList)
+
+    const newDescription = document.createElement("span");
+    newDescription.innerText = taskDescription;
+    newDescription.className = "task-name";
+
+    const newDeleteButton = document.createElement("button")
+    newDeleteButton.className = "delete-button"
+    newDeleteButton.innerText = "X"
+
+    newRow.appendChild(newDescription);
+    newRow.appendChild(newDeleteButton);
+
+    tasksList.appendChild(newRow);
+}
+
+// Empties the form textbox 
+function resetNewTaskForm() {
+    document.querySelector("#new-task-description").value = "";
+}
